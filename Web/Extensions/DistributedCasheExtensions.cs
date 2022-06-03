bbde0bbc95d0e3eb525b68d24e74edffc2a5c488
IndianBleach <84047808+IndianBleach@users.noreply.github.com>
2022-06-04 01:55:45 +0300

    Добавьте файлы проекта.

---
+++ Web/Extensions/DistributedCasheExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Web.Extensions
+{
+    public static class DistributedCasheExtensions
+    {
+        public static async Task SetRecordAsync<T>(this IDistributedCache cashe,
+            string recordId,
+            T value,
+            TimeSpan? expireTime = null,
+            TimeSpan? unusedExpireTime = null)
+        {
+            DistributedCacheEntryOptions opt = new()
+            {
+                AbsoluteExpirationRelativeToNow = expireTime ?? TimeSpan.FromSeconds(60),
+                SlidingExpiration = unusedExpireTime
+            };
+
+            var json = JsonSerializer.Serialize(value);
+            await cashe.SetStringAsync(recordId, json, opt);
+        }
+
+        public static async Task<T> GetRecordAsync<T>(this IDistributedCache cashe, 
+            string recordId)
+        {
+            var json = await cashe.GetStringAsync(recordId);
+
+            if (json is null) return default(T);
+
+            return JsonSerializer.Deserialize<T>(json);
+        }
+    }
+}
